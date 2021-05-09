7879e41a05c127962f7847d564c2641bab51d57b
eojeen01 <76842423+eojeen01@users.noreply.github.com>
2021-05-09 20:14:45 +0900

    Add Tutoring 3

---
+++ Tutoring 3/Tutoring3/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Tutoring3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelApple = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelPear = new System.Windows.Forms.Label();
+            this.labelBanana = new System.Windows.Forms.Label();
+            this.labelOrange = new System.Windows.Forms.Label();
+            this.checkBoxDelivery = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelApple
+            // 
+            this.labelApple.AutoSize = true;
+            this.labelApple.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelApple.Location = new System.Drawing.Point(200, 96);
+            this.labelApple.Name = "labelApple";
+            this.labelApple.Size = new System.Drawing.Size(91, 32);
+            this.labelApple.TabIndex = 0;
+            this.labelApple.Text = "사과 : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("나눔고딕", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(297, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 39);
+            this.textBox1.TabIndex = 1;
+            // 
+            // labelPear
+            // 
+            this.labelPear.AutoSize = true;
+            this.labelPear.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelPear.Location = new System.Drawing.Point(200, 174);
+            this.labelPear.Name = "labelPear";
+            this.labelPear.Size = new System.Drawing.Size(65, 32);
+            this.labelPear.TabIndex = 2;
+            this.labelPear.Text = "배 : ";
+            // 
+            // labelBanana
+            // 
+            this.labelBanana.AutoSize = true;
+            this.labelBanana.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelBanana.Location = new System.Drawing.Point(200, 253);
+            this.labelBanana.Name = "labelBanana";
+            this.labelBanana.Size = new System.Drawing.Size(117, 32);
+            this.labelBanana.TabIndex = 3;
+            this.labelBanana.Text = "바나나 : ";
+            // 
+            // labelOrange
+            // 
+            this.labelOrange.AutoSize = true;
+            this.labelOrange.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelOrange.Location = new System.Drawing.Point(200, 323);
+            this.labelOrange.Name = "labelOrange";
+            this.labelOrange.Size = new System.Drawing.Size(65, 32);
+            this.labelOrange.TabIndex = 4;
+            this.labelOrange.Text = "귤 : ";
+            // 
+            // checkBoxDelivery
+            // 
+            this.checkBoxDelivery.AutoSize = true;
+            this.checkBoxDelivery.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.checkBoxDelivery.Location = new System.Drawing.Point(676, 288);
+            this.checkBoxDelivery.Name = "checkBoxDelivery";
+            this.checkBoxDelivery.Size = new System.Drawing.Size(66, 25);
+            this.checkBoxDelivery.TabIndex = 5;
+            this.checkBoxDelivery.Text = "배달";
+            this.checkBoxDelivery.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("나눔고딕", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox2.Location = new System.Drawing.Point(271, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 39);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("나눔고딕", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox3.Location = new System.Drawing.Point(321, 250);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(110, 39);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("나눔고딕", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox4.Location = new System.Drawing.Point(271, 320);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(110, 39);
+            this.textBox4.TabIndex = 8;
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonResult.Location = new System.Drawing.Point(605, 161);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(207, 111);
+            this.buttonResult.TabIndex = 9;
+            this.buttonResult.Text = "Result";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            this.buttonResult.Click += new System.EventHandler(this.buttonResult_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelResult.Location = new System.Drawing.Point(407, 454);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 32);
+            this.labelResult.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 567);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBoxDelivery);
+            this.Controls.Add(this.labelOrange);
+            this.Controls.Add(this.labelBanana);
+            this.Controls.Add(this.labelPear);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelApple);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelApple;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelPear;
+        private System.Windows.Forms.Label labelBanana;
+        private System.Windows.Forms.Label labelOrange;
+        private System.Windows.Forms.CheckBox checkBoxDelivery;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button buttonResult;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Tutoring 3/Tutoring3/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutoring3
+{
+    
+    public partial class Form1 : Form
+    {
+        string delivery;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == "")
+                {
+                    textBox1.Text = "0";
+                }
+                if (textBox2.Text == "")
+                {
+                    textBox2.Text = "0";
+                }
+                if (textBox3.Text == "")
+                {
+                    textBox3.Text = "0";
+                }
+                if (textBox4.Text == "")
+                {
+                    textBox4.Text = "0";
+                }
+                if (checkBoxDelivery.Checked)
+                {
+                    delivery = "배달 O";
+                }
+                if (!checkBoxDelivery.Checked)
+                {
+                    delivery = "배달 X";
+                }
+
+                labelResult.Text = "사과 : " + int.Parse(textBox1.Text) + " 배 : " + int.Parse(textBox2.Text) + " 바나나 : " + int.Parse(textBox3.Text) + " 귤 : " + int.Parse(textBox4.Text)+" "+delivery;
+            }
+            catch
+            {
+                MessageBox.Show( "숫자로 입력해주세요.");
+            }
+            
+            
+        }
+    }
+}
